ed83b08a9d636c46f823ca99105504ac500debac
Tobias Unger <tbsunger@gmail.com>
2024-01-06 15:34:57 +0100

    Added BookingViewSet with the help of DefaultRouter

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets, permissions
+from .serializers import MenuSerializer, BookingSerializer 
+from .models import menu, booking
 
 # Create your views here.
 def index(request):
   return render(request, 'index.html', {})
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+  queryset = menu.objects.all()
+  serializer_class = MenuSerializer
+  
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+  queryset = menu.objects.all()
+  serializer_class = MenuSerializer
+  
+class BookingViewSet(viewsets.ModelViewSet):
+  queryset = booking.objects.all()
+  serializer_class = BookingSerializer
+  permission_classes = [permissions.AllowAny]
